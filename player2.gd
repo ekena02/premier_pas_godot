2ffe64289ced365b91832acaf5e8502f70f04cf2
Ekena <ekenarakotoson02@gmail.com>
2024-12-19 05:06:20 +0300

    player_animated

--- player2.gd
+++ player2.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
+var SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@onready var sprite = $sprite
+
+
+func move():
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+
+	
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -10,21 +20,27 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
+	#<>
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y +=JUMP_VELOCITY
-		
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_axis("ui_left", "ui_right")
 	#if direction:
 		#velocity.x = direction * SPEED
+	velocity.x = 0
+	move()
+	if velocity.x != 0:
+		sprite.play("move_right")
+		sprite.flip_h = velocity.x < 0
+	else:
+		sprite.play("!move")
+		
+	
+
+		
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	velocity.x = 0
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -SPEED
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
 
 	move_and_slide()
